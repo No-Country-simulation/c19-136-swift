3131c5953b6bac2772ee2dabcdcfbaa56e3575b8
dulceitamar <dulceitamar22@gmail.com>
2024-07-19 12:45:24 -0600

    Backend created

---
+++ MedicalConnect_back/src/test/java/com/c19_136_swift/MedicalConnect/MedicalConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c19_136_swift.MedicalConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
