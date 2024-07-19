7a200261081fd12a84e7045c27a05d0968895ab5
dulceitamar <dulceitamar22@gmail.com>
2024-07-19 12:45:24 -0600

    Backend created

---
+++ MedicalConnect_back/src/main/java/com/c19_136_swift/MedicalConnect/MedicalConnectApplication.java
@@ -0,0 +1,13 @@
+package com.c19_136_swift.MedicalConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalConnectApplication.class, args);
+	}
+
+}
