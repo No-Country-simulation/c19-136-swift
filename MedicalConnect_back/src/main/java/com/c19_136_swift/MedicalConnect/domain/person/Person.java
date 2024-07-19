021bb979eaf62f7397df4607ab36673fa2d514a7
dulceitamar <dulceitamar22@gmail.com>
2024-07-19 16:40:37 -0600

    Feat: Creation of a main class Person (Parent class for doctor and patient classes)

--- MedicalConnect_back/src/main/java/com/c19_136_swift/MedicalConnect/domain/person/Person.java
+++ MedicalConnect_back/src/main/java/com/c19_136_swift/MedicalConnect/domain/person/Person.java
@@ -1,2 +1,26 @@
-package com.c19_136_swift.MedicalConnect.domain.person;public class Person {
+package com.c19_136_swift.MedicalConnect.domain.person;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private StatusOnApp status;
+
 }
