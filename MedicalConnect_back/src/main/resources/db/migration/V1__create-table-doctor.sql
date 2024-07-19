56c57c0322c9b9caf29763f8eed5113101ca18e3
dulceitamar <dulceitamar22@gmail.com>
2024-07-19 16:45:02 -0600

    Feat: Creation of Doctor entity and its table

---
+++ MedicalConnect_back/src/main/resources/db/migration/V1__create-table-doctor.sql
@@ -0,0 +1,14 @@
+create table doctors(
+    id BIGSERIAL not null,
+    name varchar(120) not null,
+    email varchar(100) not null,
+    password varchar(300) not null,
+    phone_number varchar(20) not null,
+    status varchar(20) not null,
+    medical_specialty varchar(120) not null,
+    professional_license varchar(120) not null,
+    job_description TEXT,
+
+    primary key(id)
+
+);
